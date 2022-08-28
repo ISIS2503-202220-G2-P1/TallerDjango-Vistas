9772cafeee9f3b3ef6c9e0cbdd6538aae533b9d6
Juan Alegría <juanszalegria@gmail.com>
2022-08-28 17:42:43 -0500

    Feat: add CRUD operations in measurements

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
-    path('variables/', include('variables.urls'))
+    path('variables/', include('variables.urls')),
+    path('measurements/', include('measurements.urls')),
 ]
